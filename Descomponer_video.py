79254a3d24806e66c8b05a02115b21f6130dcb5c
Andrés Gil <116810726+Andrresgil@users.noreply.github.com>
2024-05-03 16:53:44 -0500

    Descomponer_video.py

--- Descomponer_video.py
+++ Descomponer_video.py
@@ -1,15 +1,16 @@
 #############
-## Codigo para descomponer el video en frames 
+## CODE TO DECOMPOSE VIDEO INTO FRAMES  
+## I RECOMMEND HAVING THE VIDEO PREVIOUSLY DOWNLOADED TO THE PC.
 
-import cv2
+import cv2 
 
-cap = cv2.VideoCapture('Video_0_Produce.mp4')
+cap = cv2.VideoCapture('Video_0_Produce.mp4') #RENAME VIDEO
 
 frame_count = 0
 
 while(cap.isOpened()):
     ret, frame = cap.read()
-    # Si no hay más fotogramas, detén la iteración
+    # If there are no more frames, stop the iteration.
     if not ret:
         break
     cv2.imwrite('Mi Viaje_{}.jpg'.format(frame_count), frame)
