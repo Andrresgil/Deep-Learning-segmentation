97145f5150be4190fd57cd22c8115f924930d02a
Andrés Gil <116810726+Andrresgil@users.noreply.github.com>
2024-05-03 17:01:02 -0500

    Reconstruccion Video.py

--- Reconstruccion Video.py
+++ Reconstruccion Video.py
@@ -1,21 +1,24 @@
+###################################################################
+## CODE TO COMPILE SEGMENTATION 
+
 import cv2
 import os
 
 Carpeta = "segmented_images/"
-files = os.listdir(Carpeta) # Obtener una lista de todos los nombres de archivo en la carpeta
-files.sort() # Ordenar
+files = os.listdir(Carpeta) # Get a list of all filenames in the folder
+files.sort() # Order
 
-# Necesito la altura y el ancho de cada frame 
+# I need the height and width of each frame  
 frame = cv2.imread(os.path.join(Carpeta, files[0]))
 height, width, channels = frame.shape
 
-fourcc = cv2.VideoWriter_fourcc(*'mp4v') # Le doy la extension que necesito 
-out = cv2.VideoWriter('video_reconstruido.mp4', fourcc, 25, (width, height)) # es 25 fotogramas para la creacion del video
+fourcc = cv2.VideoWriter_fourcc(*'mp4v') # I give you the extension I need
+out = cv2.VideoWriter('video_reconstruido.mp4', fourcc, 25, (width, height)) 
 
 for filename in files:
     img_path = os.path.join(Carpeta, filename)
     frame = cv2.imread(img_path)
 
-    out.write(frame) # Agregar el fotograma al video
+    out.write(frame) # Add frame to video
 
 out.release()
